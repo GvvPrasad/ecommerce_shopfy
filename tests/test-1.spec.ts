da34460f97c4f103d3e138da1f04b3603ca3f810
Prasad <varaprasad1819@gmail.com>
2026-04-23 09:01:54 +0530

    recored test removed

--- tests/test-1.spec.ts
+++
@@ -1,4 +0,0 @@
-await page.goto('https://automationexercise.com/product_details/6');
-await expect(page.locator('section')).toContainText('Rs. 400');
-await page.getByText('Home  Products Cart Signup').click();
-await page.getByRole('link', { name: ' Home' }).click();
