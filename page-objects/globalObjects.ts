1fe8c4c95dec7f86cd3ad9e83edea4d075491449
Prasad <varaprasad1819@gmail.com>
2026-05-04 00:53:31 +0530

    update

--- page-objects/globalObjects.ts
+++ page-objects/globalObjects.ts
@@ -4,9 +4,6 @@ export class GlobalObject {
     readonly excelFilePath = '../test-data/testData.xlsx';
     readonly register = 'register';
     readonly login = 'login';
-<<<<<<< HEAD
     readonly desiredProduct = 'Stylish Dress'
-=======
     readonly productslist = 'products'
-    readonly desiredProduct ='Stylish Dress'
 }
